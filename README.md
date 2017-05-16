30e2644783359525d9720e4689626f261d20444f
liuping02 <liuping02@meituan.com>
2017-05-16 17:32:22 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # client-service-bridge
-Bridge tools for client-service which running on phantomjs environment.
+Bridge tools for client-service which running on phantomjs environment. Must be used with "client-service".
+
+### Install
+```
+npm install client-service-bridge
+```
+
+### Demo
+```
+var clientTools = require('client-service-bridge');
+var webPage = require('webpage');
+var page = webPage.create();
+
+page.viewportSize = {
+    width: 1920,
+    height: 1080
+};
+
+page.open('https://github.com', function (status) {
+  var base64 = page.renderBase64('PNG');
+  clientTools.send(base64, function (status, res) {
+    phantom.exit();
+  });
+});
+```
+
+### API
+
+#### clientTools.send(data, cbk(status, res));
+* data: Data which send to node.
+* cbk(status, res):
+	* status: response status(`success`/`fail`).
+	* res: response data.
---
+++ bridge-client.js
@@ -0,0 +1,107 @@
+var webPage = require('webpage');
+var system = require('system');
+
+var params = system.args;
+var clientConfig = {
+    secret: params[4],
+    api: params[3]
+};
+
+var initEnv = function (page) {
+    if (clientConfig.secret) {
+        page.customHeaders = {
+            'X-Secret': clientConfig.secret
+        };
+    }
+};
+
+var bridgeId = system.args[2];
+
+var rfc3986 = function (str) {
+  return str.replace(/[!'()*]/g, function (unit) {
+    return '%' + unit.charCodeAt(0).toString(16).toUpperCase()
+  })
+};
+
+var paramsToQuery = function (params) {
+    var queryStrs = [];
+    for (var i in params) {
+        if (params.hasOwnProperty(i)) {
+            queryStrs.push(i + '=' + params[i]);
+        }
+    }
+    return rfc3986(queryStrs.join('&'));
+};
+
+var concatUrlWidthObj = function (url, obj) {
+    var urlQueryFlagPos = url.indexOf('?');
+    if (urlQueryFlagPos === -1) {
+        url += '?';
+    }
+    else if ((url.length - 1) !== urlQueryFlagPos) {
+        url += '&';
+    }
+    url += paramsToQuery(obj);
+    return url;
+};
+
+var send = function (data, cbk) {
+    if (!data) {
+        return cbk && cbk();
+    }
+    var postPage = webPage.create();
+    initEnv(postPage);
+    if (typeof data !== 'object') {
+        data = {data: data};
+    }
+    var settings = {
+        operation: 'POST',
+        encoding: 'utf8',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-BridgeId': bridgeId
+        },
+        data: JSON.stringify(data)
+    };
+    postPage.open(
+        clientConfig.api,
+        settings,
+        function (status) {
+            cbk && cbk(status, postPage.plainText);
+            setTimeout(function () {
+                postPage.close();
+            }, 500);
+        }
+    );
+};
+
+var get = function (data, cbk) {
+    if (!data) {
+        return cbk && cbk();
+    }
+    var getterPage = webPage.create();
+    initEnv(getterPage);
+    var requestUrl = concatUrlWidthObj(clientConfig.api, data);
+    var settings = {
+        encoding: 'utf8',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-BridgeId': bridgeId
+        }
+    };
+    getterPage.open(
+        requestUrl,
+        settings,
+        function (status) {
+            cbk && cbk(status, getterPage.plainText);
+            setTimeout(function () {
+                getterPage.close();
+            }, 500);
+        }
+    );
+};
+
+module.exports = {
+    send: send,
+    get: get
+};
